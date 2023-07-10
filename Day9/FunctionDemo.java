6aa102d9770ba766f7e65c96938e7d32b8b3193c
Shoyeb <toshoyeb@gmail.com>
2023-07-10 12:54:15 +0530

    day9

---
+++ Day9/FunctionDemo.java
@@ -0,0 +1,22 @@
+package Day9;
+
+import java.net.StandardSocketOptions;
+
+public class FunctionDemo {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Trial");
+        int one =10;
+        int two =20;
+        int res = Demoscopes(one,two);
+        System.out.println(res);
+    }
+    public static int Demoscopes(int a,int b)
+    {
+        int sum= a+b;
+        return sum;
+    }
+
+}
+w
